28c62545c21a5642345243eb921feb8641391190
Jose Enrique Marquez <jmrqz21@gmail.com>
2020-03-18 17:51:44 -0500

    add fetch

--- src/page/home.js
+++ src/page/home.js
@@ -1,12 +1,20 @@
-const home = () => {
+import getData from './../utils/getData';
+
+const home = async () => {
+  const characters  = await getData();
+
   const view = `
     <div class="Characters">
-      <article class="Character-item">
-        <a href="#/1/">
-          <img src="image" alt="name" />
-          <h2>Name</h2>
-        </a>
-      </article>
+      ${characters.results.map((character) => {
+        return `
+          <article class="Character-item">
+            <a href="#/${character.id}/">
+              <img src="${character.image}" alt="${character.name}" />
+              <h2>${character.name}</h2>
+            </a>
+          </article>
+        `
+      }).join('')}
     </div>
   `;
 
